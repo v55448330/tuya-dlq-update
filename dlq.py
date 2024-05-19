2d843aa9c4622c7a7a2fb93b1c41ed90fd84f465
XiaoBao <zhang@xiaobao.cool>
2024-05-19 21:22:13 +0800

    add mqtt reconnect

--- dlq.py
+++ dlq.py
@@ -23,9 +23,26 @@ dlq_current_gauge = Gauge('xiaobao_home_dlq_current', 'dlq current(A)', ['phase'
 dlq_power_gauge = Gauge('xiaobao_home_dlq_power', 'dlq power(kW)', ['phase'])
 dlq_total_energy_gauge = Gauge('xiaobao_home_dlq_total_energy', 'dlq total energy(W)')
 
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
-client.connect(MQTT_BROKER, int(MQTT_PORT), 30)
+def mqtt_on_disconnect(client, userdata, rc):
+    if rc != 0:
+        while True:
+            try:
+                client.reconnect()
+                break
+            except:
+                time.sleep(10)
+                pass
+
+def init_mqtt():
+    try:
+        client = mqtt.Client()
+        client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+        client.connect(MQTT_BROKER, int(MQTT_PORT), 2)
+        client.on_disconnect = mqtt_on_disconnect
+        client.loop_start()
+        return client
+    except Exception as e:
+        print(e)
 
 def get_dlq_status():
     c = tinytuya.Cloud(
@@ -60,14 +77,15 @@ def get_dlq_status():
         print(e)
         return {}
 
-def send_mqtt(topic, payload):
+def send_mqtt(topic, payload, client):
+    print('state: ', client._state, 'loop: ', client._thread, end = ' ')
     try:
         topic = topic
         payload = payload
         qos = 0
         client.publish(topic, payload, qos)
         time.sleep(1)
-    except Expection as e:
+    except Exception as e:
         print(e)
         print(topic)
         print(payload)
@@ -105,44 +123,55 @@ def build_discovery_payload(item):
     }
     return topic, payload
 
-def ecorehome_discovery():
+def ecorehome_discovery(client):
     dlq_status = get_dlq_status()
     for phase in dlq_status["phase"]:
         for phase_item in dlq_status["phase"][phase].keys():
             discovery_payload = build_discovery_payload("{phase}_{phase_item}".format(phase=phase, phase_item=phase_item))
             print(discovery_payload)
 
-            send_mqtt(discovery_payload[0], json.dumps(discovery_payload[1]))
+            send_mqtt(discovery_payload[0], json.dumps(discovery_payload[1]), client)
 
     discovery_payload = build_discovery_payload("total_forward_energy")
     print(discovery_payload)
 
-    send_mqtt(discovery_payload[0], json.dumps(discovery_payload[1]))
+    send_mqtt(discovery_payload[0], json.dumps(discovery_payload[1]), client)
 
-def metrics_update():
+def metrics_update(client):
     while True:
-        dlq_status = get_dlq_status()
-        if dlq_status:
-            for phase in dlq_status["phase"]:
-                state = dlq_status["phase"][phase]['voltage']
-                dlq_voltage_gauge.labels(phase).set(state)
-                send_mqtt("dlq/{item}_voltage/state".format(item=phase), json.dumps({"voltage": state}))
+        try:
+            dlq_status = get_dlq_status()
+            if dlq_status:
+                for phase in dlq_status["phase"]:
+                    state = dlq_status["phase"][phase]['voltage']
+                    dlq_voltage_gauge.labels(phase).set(state)
+                    send_mqtt("dlq/{item}_voltage/state".format(item=phase), json.dumps({"voltage": state}), client)
 
-                state = dlq_status["phase"][phase]['current']
-                dlq_current_gauge.labels(phase).set(dlq_status["phase"][phase]['current'])
-                send_mqtt("dlq/{item}_current/state".format(item=phase), json.dumps({"current": state}))
+                    state = dlq_status["phase"][phase]['current']
+                    dlq_current_gauge.labels(phase).set(dlq_status["phase"][phase]['current'])
+                    send_mqtt("dlq/{item}_current/state".format(item=phase), json.dumps({"current": state}), client)
 
-                state = dlq_status["phase"][phase]['power']
-                dlq_power_gauge.labels(phase).set(dlq_status["phase"][phase]['power'])
-                send_mqtt("dlq/{item}_power/state".format(item=phase), json.dumps({"loadpower": float(state) * 1000}))
+                    state = dlq_status["phase"][phase]['power']
+                    dlq_power_gauge.labels(phase).set(dlq_status["phase"][phase]['power'])
+                    send_mqtt("dlq/{item}_power/state".format(item=phase), json.dumps({"loadpower": float(state) * 1000}), client)
 
-            state = dlq_status["total_forward_energy"] 
-            dlq_total_energy_gauge.set(dlq_status["total_forward_energy"])
-            send_mqtt("dlq/total_forward_energy/state".format(item=phase), json.dumps({"powerconsumed": float(state) / 100}))
+                state = dlq_status["total_forward_energy"] 
+                dlq_total_energy_gauge.set(dlq_status["total_forward_energy"])
+                send_mqtt("dlq/total_forward_energy/state".format(item=phase), json.dumps({"powerconsumed": float(state) / 100}), client)
+
+        except Exception as e:
+            print(e)
 
         time.sleep(int(update_interval))
 
 if __name__ == '__main__':
     start_http_server(int(LISTEN_PORT))
-    ecorehome_discovery()
-    metrics_update()
+
+    client = init_mqtt()
+    if not client:
+        while True:
+            client = init_mqtt()
+            time.sleep(10)
+
+    ecorehome_discovery(client)
+    metrics_update(client)
